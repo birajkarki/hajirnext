5b9ac808657a6555af0022ab9741dc33adf20db0
pramisha <pramisha1209@gmail.com>
2024-03-04 07:30:02 +0545

    sidebars, steppers

--- components/Sidebar/CompanySidebar.jsx
+++ components/Sidebar/CompanySidebar.jsx
@@ -16,17 +16,21 @@ import LogoutButton from "./LogoutButton";
 import BusinessIcon from "@mui/icons-material/Business";
 import Link from "next/link";
 import ExpandLess from "@mui/icons-material/ExpandLess";
+import PersonIcon from '@mui/icons-material/Person';
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import Collapse from "@mui/material/Collapse";
 import { useParams } from "next/navigation";
 import { Button } from "@mui/material";
 import TestProfileCard from "../testprofile/TestProfileCard";
 import { getRequest } from "@/services/ApiRequestService";
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import SsidChartIcon from '@mui/icons-material/SsidChart';
 import { useRouter } from "next/navigation";
 
 const CompanySidebar = () => {
   const [openSettings, setOpenSettings] = useState(false);
   const [openReport, setOpenReport] = useState(false); // Add this line
+  const [activeLink, setActiveLink] = useState("");
   const { companyId } = useParams();
 
   const onLogoutClick = async (e) => {
@@ -37,7 +41,9 @@ const CompanySidebar = () => {
       return router.push("/login");
     }
   };
-
+  const handleLinkClick = (text) => {
+    setActiveLink(text);
+  };
   const LINKS = [
     {
       text: "Home",
@@ -47,26 +53,28 @@ const CompanySidebar = () => {
     {
       text: "Employee",
       href: `/dashboard/company/${companyId}/`,
-      icon: HomeIcon,
+      icon: '/employee.svg',
     },
     {
       text: "Attendance",
       href: `/dashboard/company/${companyId}/attendance/`,
-      icon: HomeIcon,
+   
+      icon:'/attendance.svg'
     },
     {
       text: "Report",
-      icon: HomeIcon,
+      icon: '/registerr.svg',
       sublinks: [
         {
           text: "Activity Report",
           href: `/dashboard/company/${companyId}/activityreport/`,
-          icon: BusinessIcon,
+          icon: '/tabler.svg',
+
         },
         {
           text: "Payments Reports",
           href: `/dashboard/company/${companyId}/paymentreport/`,
-          icon: StarIcon,
+          icon: '/moneyy.svg',
         },
       ],
     },
@@ -78,32 +86,32 @@ const CompanySidebar = () => {
           text: "Messenger Inbox",
           href: `/dashboard/company/${companyId}/messaginginbox/`,
 
-          icon: BusinessIcon,
+          icon: '/Message.svg ',
         },
 
         {
           text: "Missing Attendance",
           href: `/dashboard/company/${companyId}/missingattendance/`,
 
-          icon: ChecklistIcon,
+          icon: '/missingattendance.svg',
         },
         {
           text: "Missing Leave",
           href: `/dashboard/company/${companyId}/missingleave/`,
 
-          icon: ChecklistIcon,
+          icon: '/missingLeave.svg',
         },
         {
           text: "Add Approval",
           href: `/dashboard/company/${companyId}/addapproval/`,
 
-          icon: SettingsIcon,
+          icon: '/AddApprover.svg',
         },
         {
           text: "Update Holiday",
           href: `/dashboard/company/${companyId}/updateholiday/`,
 
-          icon: SettingsIcon,
+          icon: '/updateHolidays.svg',
         },
       ],
     },
@@ -142,17 +150,26 @@ const CompanySidebar = () => {
               <ListItemButton
                 component={href ? Link : undefined}
                 href={href}
-                onClick={
+                           onClick={
                   sublinks
                     ? text === "Report"
                       ? handleReportClick
                       : handleSettingsClick
-                    : undefined
+                    : () => handleLinkClick(text)
                 }
+                // sx={activeLink === text ? { backgroundColor: "#22408B15" } : {}}
+                sx={{
+                  "&:hover, &:active": {
+                    backgroundColor: "#22408B15", // Change background color on hover and click
+                  },
+                }}
               >
-                <ListItemIcon>
-                  <Icon />
-                </ListItemIcon>
+            
+                {typeof Icon === "string" ? (
+                    <img src={Icon} alt={text} style={{ marginRight: "18px" }} />
+                  ) : (
+                    <Icon style={{ marginLeft:'-3px',marginRight: "16px" }}  />
+                  )}
                 <ListItemText primary={text} />
                 {sublinks &&
                   (text === "Report" ? (
@@ -179,11 +196,23 @@ const CompanySidebar = () => {
                     <ListItemButton
                       component={href ? Link : undefined}
                       href={href}
-                      sx={{ pl: 4 }}
-                    >
-                      <ListItemIcon>
-                        <SubIcon />
-                      </ListItemIcon>
+                      onClick={() => handleLinkClick(text)} // Set active link when clicked
+                      sx={{
+                      pl: 4, 
+                      // ...(activeLink === text && { backgroundColor: "#22408B15" }) 
+                      "&:hover, &:active": {
+                        backgroundColor: "#22408B15", // Change background color on hover and click
+                      },
+                    }}
+                      >
+                   
+                      {typeof SubIcon === "string" ? (
+                        <img src={SubIcon} alt={text} style={{ marginRight: "8px" }}/>
+                      ) : (
+                        <ListItemIcon>
+                          <SubIcon />
+                        </ListItemIcon>
+                      )}
                       <ListItemText primary={text} />
                     </ListItemButton>
                   </ListItem>
